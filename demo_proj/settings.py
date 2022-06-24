4b8f5fb85b664fd4a138930c6c38b220dee84a32
keran-w <83668340+keran-w@users.noreply.github.com>
2022-06-24 11:03:52 +0800

    update index

--- demo_proj/settings.py
+++ demo_proj/settings.py
@@ -120,5 +120,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
-    '/var/www/static/',
 )
+
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- demo_proj/views.py
+++ demo_proj/views.py
@@ -1,6 +1,27 @@
+from calendar import c
+from multiprocessing import context
+import pandas as pd
 from django.shortcuts import render
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
+
 
 def index(request):
-    context = {}
-    context['name'] = "Hello World!"
-    return render(request, 'index.html', context)
+    print(request)
+
+    if request.method == 'POST':
+        # and request.FILES['data']
+        data = request.FILES['data']
+        print()
+        print()
+        print(data)
+        print()
+        print()
+        fs = FileSystemStorage()
+        filename = fs.save(data.name, data)
+        data_html = pd.read_excel(filename).head(20).to_html()
+        return render(request, 'index.html', {
+            data: data_html,
+        })
+
+    return render(request, 'index.html')
